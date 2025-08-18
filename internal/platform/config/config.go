7bdc5eda2bb65615202dcc3598406d9816d1abe7
mdeadwiler <mdeadwiler85@gmail.com>
2025-08-18 18:05:00 -0400

    Add initial configuration structs
    Introduces config structs for server, database, OpenSky API, and logging settings. These provide a foundation for managing application configuration in a structured way.

---
+++ internal/platform/config/config.go
@@ -0,0 +1,47 @@
+package config
+
+import (
+	"os"
+	"strconv"
+	"time"
+)
+
+type config struct {
+	Server  ServerConfig
+	Dartabase DatabaseConfig
+	OpenSky OpenSkyConfig
+	Logging LoggingConfig
+}
+
+type ServerConfig struct {
+	Port  string
+	ReadTimeout time.Duration
+	WriteTimeout time.Duration
+	IdleTimeOut time.Duration
+	ShutdownTimeout time.Duration
+}
+
+type DatabaseConfig struct {
+	Host string 
+	Port string
+	User string
+	Password string
+	DBName string
+	SSLMode string
+	MaxOpenConns int
+	MaxIdleConns int
+	MaxLifetime time.Duration 
+}
+
+type OpenSkyConfig struct {
+	BaseUrl string
+	User string
+	Password string
+	Timeout time.Duration
+	RateLimit int // This will be request per minute
+}
+
+type LoggingConfig struct {
+	Level string
+	Format string // "json"
+}
