e132ff8ce983f2b58e4a31cc028176444f13b368
mdeadwiler <mdeadwiler85@gmail.com>
2025-08-20 00:44:52 -0400

    Add position and movement fields to Flight struct
    Extended the Flight struct with longitude, latitude, altitude, velocity, heading, and vertical rate fields to support more detailed flight data.

--- internal/domain/flight.go
+++ internal/domain/flight.go
@@ -9,4 +9,18 @@ type Flight struct {
 	ICAO24 string `json:"icao24"` // Transponder address
 	Callsign *string `json:"callsign"` // This can be null for flight 
 	Country string `json:"country"` // Registration location
+
+	// Position Data
+	Longitude *float64 `json:"longitude"` // this will be in decimal degrees
+	Latitude *float64 `json:"latitude"` // this will be in decimal degrees
+
+	// Altitude Data and this will be converted in feet in the adapter layer
+	BarometricAltitude *float64 `json:"barometric_altitude"` 
+	GeometricAltitude *float64 `json:"geometric_altitude"`
+
+	// Movement Data
+Velocity *float64 `json:"velocity"`
+Heading *float64 `json:"heading"`
+VerticalRate *float64 `json:"vertical_rate"`
 }
+
