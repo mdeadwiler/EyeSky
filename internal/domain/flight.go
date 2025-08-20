c59dd724d0f5a38c9e10c035265d25d2faa393d3
mdeadwiler <mdeadwiler85@gmail.com>
2025-08-20 00:27:34 -0400

    Define Flight struct in domain package
    Introduced the Flight struct with ICAO24, optional Callsign, and Country fields to represent aircraft identification details.

--- internal/domain/flight.go
+++ internal/domain/flight.go
@@ -1,3 +1,12 @@
 package domain
 
-// Flight entity placeholder
+import (
+	"time"
+)
+
+// Aircract Identification
+type Flight struct {
+	ICAO24 string `json:"icao24"` // Transponder address
+	Callsign *string `json:"callsign"` // This can be null for flight 
+	Country string `json:"country"` // Registration location
+}
